c59d6f27fa7cb081fabeff50cf5432727595eefa
roobeen-b <rubinbaidhya@gmail.com>
2024-11-18 16:26:46 +0545

    Implemented user logout functionality

--- src/components/admin/header.jsx
+++ src/components/admin/header.jsx
@@ -1,8 +1,23 @@
 /* eslint-disable react/prop-types */
 import { LogOut, Menu } from "lucide-react";
 import { Button } from "../ui/button";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "@/store/auth-slice";
+import { toast } from "@/hooks/use-toast";
 
 const AdminHeader = ({ setOpen }) => {
+  const dispatch = useDispatch();
+
+  function handleUserLogout() {
+    dispatch(logoutUser()).then((res) => {
+      if (res?.payload?.success) {
+        toast({
+          title: res.payload.message,
+        });
+      }
+    });
+  }
+
   return (
     <header className="flex justify-between items-center py-4 px-4 bg-background border-b">
       <div>
@@ -11,7 +26,10 @@ const AdminHeader = ({ setOpen }) => {
         </Button>
       </div>
       <div>
-        <Button className="flex flex-1 shadow rounded-sm">
+        <Button
+          className="flex flex-1 shadow rounded-sm"
+          onClick={handleUserLogout}
+        >
           <LogOut />
           Logout
         </Button>
